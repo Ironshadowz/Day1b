92d8cd79056b442bf0a75e83bdab496ee04f5391
ironshadowz <chenwl911@gmail.com>
2023-04-17 23:45:05 +0800

    initial commit

---
+++ App.java
@@ -0,0 +1,30 @@
+package sg.edu.nus.iss;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Console con = System.console();
+        List<String> todos = new ArrayList<String>();
+        String input="";
+        while(!input.equals("quit"))
+        {
+            input= con.readLine("Enter a string: ");
+            if(!input.equals("quit")){
+            todos.add(input);
+            }
+        }
+        for(String todo: todos)
+        {
+            System.out.println("To do task: "+todo);
+        }
+    }
+}
